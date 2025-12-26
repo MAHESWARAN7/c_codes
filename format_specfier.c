b502aecdaa330f7295adfab09e34a837f89fa945
MAHESWARAN7 <smaheswaran7002@gmail.com>
2025-12-26 23:00:51 +0530

    new

--- format_specfier.c
+++ format_specfier.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main() {
+    
     int num = 255;
+    int numa = 1000;
+    int numb = 10000;
+    int numc = 100000;
     float fnum = 123.456789;
     char ch = 'A';
     char str[] = "Hello, World!";
 
+
     // Using different format specifiers
+    
+    
     printf("Integer: %d\n", num);               // %d for integer
+    printf("%4d\n%4d\n%4d\n", numa, numb, numc); // %4d for width of 4
     printf("Hexadecimal: %x\n", num);           // %x for hexadecimal
     printf("Floating point: %.2f\n", fnum);     // %.2f for float with 2 decimal places
     printf("Character: %c\n", ch);              // %c for character
