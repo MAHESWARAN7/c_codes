3937cdbfd3f81ace78bb47438640db1b99a61d26
MAHESWARAN7 <smaheswaran7002@gmail.com>
2025-12-27 20:04:31 +0530

    user input added

--- arithmetic.c
+++ arithmetic.c
@@ -9,10 +9,14 @@ int main() {
     float quot = (float)b / a;
     int c = 50;
     int d=2;
+    
+    
     a=a+5; // a = 15
     b+=5; // b = 25
     c-=10; // c = 40
     d *= a; // d = 200
+    
+    
     printf("a=%d\n",a);
     printf("b=%d\n",b);
     printf("c=%d\n",c);
