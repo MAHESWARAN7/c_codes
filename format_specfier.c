fdd95b8570099110f540235f5677a0882e7f88d0
MAHESWARAN7 <smaheswaran7002@gmail.com>
2025-12-27 17:39:47 +0530

    add

--- format_specfier.c
+++ format_specfier.c
@@ -3,7 +3,7 @@ int main() {
     
     int num = 255;
     int numa = 1000;
-    int numb = 10000;
+    int numb = -10000;
     int numc = 100000;
     float fnum = 123.45456;
     char ch = 'A';
@@ -15,6 +15,7 @@ int main() {
     
     printf("Integer: %d\n", num);               // %d for integer
     printf("%4d\n%4d\n%4d\n", numa, numb, numc); // %4d for width of 4
+    printf("%+d\n%+d\n%+d\n%+d\n", numa,numb,numc);
     printf("Hexadecimal: %x\n", num);           // %x for hexadecimal
     printf("Floating point: %.2f\n", fnum);     // %.2f for float with 2 decimal places
     printf("Character: %c\n", ch);              // %c for character
