8fa788c3cd37b005d0fcfb4c832963085654a2cd
MAHESWARAN7 <smaheswaran7002@gmail.com>
2025-12-30 23:00:17 +0530

    user

--- user_inputs.c
+++ user_inputs.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 int main (){
-    int a;
-    scanf(" %d",&a);
-    printf("here %d",a);
-return 0;
+    int    a;
+    float  b;
+    char   c;
+    double d;
+    char   e[30];
 
+
+    printf("your no. : ",a);
+    scanf("%d",&a);
+
+    printf("your gpa : ",b);
+    scanf("%f", &b);
+
+    printf("your grade : ",c);
+    scanf(" %c", &c);
+
+    printf("your attendence : ",d);
+    scanf("%lf",&d);
+    
+    getchar();
+    printf("your name :", e);
+    fgets(e,sizeof(e),stdin );
+    e[strlen(e)-1]='\0';
+
+    printf("your name is : %s\n",e);
+    printf("your no. is :%d\n",a);
+    printf("your gpa is : %.2f\n",b);
+    printf("your grade is: %c\n",c);
+    printf("your attendence is : %.2lf\n",d);
+    return 0 ;
 }
